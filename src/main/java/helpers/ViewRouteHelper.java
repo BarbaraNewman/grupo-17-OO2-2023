aca835cb3ff242687ed8fa718dcda09ec9f70467
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-07 01:37:33 -0300

    Eliminar ViewRouteHelper.java

--- src/main/java/helpers/ViewRouteHelper.java
+++
@@ -1,9 +0,0 @@
-package helpers;
-
-public class ViewRouteHelper {
-	//Views
-	public final static String INDEX = "home/index";  // nombre del template .html
-	
-	//Redirects
-	public final static String ROUTE_INDEX = "/index";
-}
