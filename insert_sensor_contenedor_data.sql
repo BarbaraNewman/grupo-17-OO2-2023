e7aac4eb89268ad8fdc3061e15938cbaca13c72e
Sergio Robles <mpthrash@gmail.com>
2023-06-18 19:24:48 -0300

    Create insert_sensor_contenedor_data.sql

---
+++ insert_sensor_contenedor_data.sql
@@ -0,0 +1,9 @@
+USE `grupo-17-oo2-2023`;
+
+INSERT INTO sensor_contenedor (id_sensor, modelo, nivel_bateria, nivel_llenado, numero_serie)
+VALUES
+('1', 'Eco-Bits 20', '80', '60', '20-9876543210'),
+('2', 'Eco-Bits 20', '45', '30', '20-2468135790'),
+('3', 'Eco-Bits 20', '90', '75', '20-1122334455'),
+('4', 'Eco-Bits 20', '60', '20', '20-987654321'),
+('5', 'Eco-Bits 20', '75', '90', '20-5432109876');
