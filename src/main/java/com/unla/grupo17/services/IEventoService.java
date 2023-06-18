b060232ab2a7c565593fcd9421358410524398d0
Sergio Robles <mpthrash@gmail.com>
2023-06-18 19:48:37 -0300

    Creacion Repo y Service de Evento

---
+++ src/main/java/com/unla/grupo17/services/IEventoService.java
@@ -0,0 +1,15 @@
+package com.unla.grupo17.services;
+
+import java.util.List;
+
+import com.unla.grupo17.entities.Evento;
+
+public interface IEventoService {
+
+	List<Evento> getAll();
+
+	public abstract Evento findByIdEvento(int idEvento);
+
+	public Evento insertOrUpdate(Evento evento);
+
+}
