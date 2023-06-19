bd6ed0ce0b479cbd909857a600da48248e291707
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-19 19:33:13 -0300

    Create SensorAlumbradoService.java

---
+++ src/main/java/com/unla/grupo17/services/implementation/SensorAlumbradoService.java
@@ -0,0 +1,31 @@
+package com.unla.grupo17.services.implementation;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import com.unla.grupo17.entities.SensorAlumbrado;
+import com.unla.grupo17.repositories.IAlumbradoRepository;
+import com.unla.grupo17.repositories.ISensorAlumbradoRepository;
+import com.unla.grupo17.services.ISensorAlumbradoService;
+
+@Service("sensorAlumbradoService")
+public class SensorAlumbradoService implements ISensorAlumbradoService {
+
+	@Autowired
+	@Qualifier("sensorAlumbradoRepository")
+	private ISensorAlumbradoRepository sensorAlumbradoRepository;
+
+	@Override
+	public List<SensorAlumbrado> getAll() {
+		return sensorAlumbradoRepository.findAll();
+	}
+
+	@Override
+	public SensorAlumbrado findByIdSensor(int idSensor) {
+		return sensorAlumbradoRepository.findByIdSensor(idSensor);
+	}
+
+}
