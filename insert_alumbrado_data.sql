b193f7531748d8e7641c77cb4f85479f5647eb3b
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-19 19:31:53 -0300

    Create insert_alumbrado_data.sql

---
+++ insert_alumbrado_data.sql
@@ -0,0 +1,10 @@
+USE `grupo-17-oo2-2023`;
+
+INSERT INTO alumbrado
+(estado_luces, tiempo_apagado, tiempo_encendido, umbral_luminosidad, id_dispositivo)
+VALUES
+(true, 8, 8, 80, 1),
+(false, 8, 8, 10, 2),
+(true, 8, 8, 40, 3),
+(false, 8, 8, 10, 4),
+(true, 8, 8, 80, 5);
