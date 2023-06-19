27fd7a76bf6fdd59766705a4df21c559553264f1
Sergio Robles <mpthrash@gmail.com>
2023-06-18 22:23:31 -0300

    Update inserts.sql de Contenedor y Sensor

--- insert_contenedor_data.sql
+++ insert_contenedor_data.sql
@@ -1,10 +1,10 @@
 USE `grupo-17-oo2-2023`;
 
 INSERT INTO contenedor
-(capacidad, lleno, reciclable, id_dispositivo, id_sensor)
+(capacidad, lleno, reciclable, id_dispositivo)
 VALUES
-(100, true, true, 1, 1),
-(200, false, false, 2, 2),
-(150, true, true, 3, 3),
-(250, false, false, 4, 4),
-(180, true, true, 5, 5);
+(100, true, true, 1),
+(200, false, false, 2),
+(150, true, true, 3),
+(250, false, false, 4),
+(180, true, true, 5);
