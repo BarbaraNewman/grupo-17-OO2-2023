650ee70f992074b05cfee6c6723a7128a6f6a711
Sergio Robles <mpthrash@gmail.com>
2023-06-18 22:23:08 -0300

    Important Update entities Contenedor y Sensor
    Contenedor ya no posee la property Sensor, sino que SensorContenedor posee la property de Contenedor.

--- src/main/java/com/unla/grupo17/entities/Contenedor.java
+++ src/main/java/com/unla/grupo17/entities/Contenedor.java
@@ -1,22 +1,17 @@
 package com.unla.grupo17.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
 public class Contenedor extends Dispositivo {
 	private boolean reciclable;
 	private int capacidad;
 	private boolean lleno;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "idSensor", nullable=true)
-	private SensorContenedor sensor;
 
 }
