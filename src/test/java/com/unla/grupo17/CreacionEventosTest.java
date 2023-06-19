8637d5d476c115549aee2580e3badec8d318b30b
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-19 19:34:25 -0300

    Update CreacionEventosTest.java

--- src/test/java/com/unla/grupo17/CreacionEventosTest.java
+++ src/test/java/com/unla/grupo17/CreacionEventosTest.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.unla.grupo17.entities.Evento;
+import com.unla.grupo17.entities.SensorAlumbrado;
 import com.unla.grupo17.entities.SensorContenedor;
 import com.unla.grupo17.services.IEventoService;
+import com.unla.grupo17.services.ISensorAlumbradoService;
 import com.unla.grupo17.services.ISensorContenedorService;
 
 @SpringBootTest
@@ -17,6 +19,9 @@ class CreacionEventosTest {
 
 	@Autowired
 	private ISensorContenedorService sensorContenedorService;
+
+	@Autowired
+	private ISensorAlumbradoService sensorAlumbradoService;
 
 	@Autowired
 	private IEventoService eventoService;
@@ -37,6 +42,15 @@ class CreacionEventosTest {
 
 			///////////////////////////////////
 
+		}
+
+		List<SensorAlumbrado> metricasSensorAlumbrado = sensorAlumbradoService.getAll();
+		for (SensorAlumbrado sensorAlumbrado : metricasSensorAlumbrado) {
+			Evento evento = new Evento();
+			evento.setFechaHoraRegistro(LocalDateTime.now());
+			evento.setDescripcion(sensorAlumbrado.toString());
+			evento.setDispositivo(sensorAlumbrado.getAlumbrado());
+			eventoService.insertOrUpdate(evento);
 		}
 	}
 
