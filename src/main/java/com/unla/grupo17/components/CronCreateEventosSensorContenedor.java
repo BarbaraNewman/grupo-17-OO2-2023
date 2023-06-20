a622237957b469fe8621788518f4b3838af353d5
Sergio Robles <mpthrash@gmail.com>
2023-06-20 09:20:59 -0300

    Fix Creacion EventosContenedor CronoRepeat

--- src/main/java/com/unla/grupo17/components/CronCreateEventosSensorContenedor.java
+++ src/main/java/com/unla/grupo17/components/CronCreateEventosSensorContenedor.java
@@ -1,19 +1,19 @@
-package com.unla.grupo17;
+package com.unla.grupo17.components;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.unla.grupo17.entities.Evento;
 import com.unla.grupo17.entities.SensorContenedor;
 import com.unla.grupo17.services.IEventoService;
 import com.unla.grupo17.services.ISensorContenedorService;
 
-@SpringBootTest
-class CreacionEventosTest {
+@Component
+public class CronCreateEventosSensorContenedor {
 
 	@Autowired
 	private ISensorContenedorService sensorContenedorService;
@@ -21,23 +21,25 @@ class CreacionEventosTest {
 	@Autowired
 	private IEventoService eventoService;
 
-	// @Test
-	// void contextLoads() {}
-
-	@Test
-	public void leerMetricas() {
-		// Lectura y creacion de Eventos para los Contenedores
+	// Tarea programada
+	@Scheduled(fixedDelay = 60000) // 1 minuto // @Hourly
+	public void leerMetricasCrearEventos() {
+		// Se leen las metricas
 		List<SensorContenedor> metricasSensorContenedor = sensorContenedorService.getAll();
+
+		// Por cada metrica, se crea un evento
 		for (SensorContenedor sensorContenedor : metricasSensorContenedor) {
 			Evento evento = new Evento();
 			evento.setFechaHoraRegistro(LocalDateTime.now());
-			evento.setDescripcion(sensorContenedor.toString());
 			evento.setDispositivo(sensorContenedor.getContenedor());
+
+			evento.setDescripcion(sensorContenedor.toString());
+
 			eventoService.insertOrUpdate(evento);
 
 			///////////////////////////////////
 
 		}
-	}
 
+	}
 }
