0dae87d26a080d225c46e136a1376a9f5ff22eb4
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-21 15:31:20 -0300

    Update CreacionEventosTest.java

--- src/test/java/com/unla/grupo17/CreacionEventosTest.java
+++ src/test/java/com/unla/grupo17/CreacionEventosTest.java
@@ -9,16 +9,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import com.unla.grupo17.entities.Evento;
 import com.unla.grupo17.entities.SensorAlumbrado;
-import com.unla.grupo17.entities.SensorContenedor;
 import com.unla.grupo17.services.IEventoService;
 import com.unla.grupo17.services.ISensorAlumbradoService;
-import com.unla.grupo17.services.ISensorContenedorService;
 
 @SpringBootTest
 class CreacionEventosTest {
-
-	@Autowired
-	private ISensorContenedorService sensorContenedorService;
 
 	@Autowired
 	private ISensorAlumbradoService sensorAlumbradoService;
@@ -26,29 +21,30 @@ class CreacionEventosTest {
 	@Autowired
 	private IEventoService eventoService;
 
-	// @Test
-	// void contextLoads() {}
-
 	@Test
 	public void leerMetricas() {
-		// Lectura y creacion de Eventos para los Contenedores
-		List<SensorContenedor> metricasSensorContenedor = sensorContenedorService.getAll();
-		for (SensorContenedor sensorContenedor : metricasSensorContenedor) {
-			Evento evento = new Evento();
-			evento.setFechaHoraRegistro(LocalDateTime.now());
-			evento.setDescripcion(sensorContenedor.toString());
-			evento.setDispositivo(sensorContenedor.getContenedor());
-			eventoService.insertOrUpdate(evento);
-
-			///////////////////////////////////
-
-		}
 
 		List<SensorAlumbrado> metricasSensorAlumbrado = sensorAlumbradoService.getAll();
 		for (SensorAlumbrado sensorAlumbrado : metricasSensorAlumbrado) {
+
 			Evento evento = new Evento();
+
+			if (sensorAlumbrado.getUmbralLuminosidad() <= 20) {
+				sensorAlumbrado.getAlumbrado().setEstadoLuces(true);
+			} else if (sensorAlumbrado.getUmbralLuminosidad() >= 80) {
+				sensorAlumbrado.getAlumbrado().setEstadoLuces(false);
+			}
+
+			if (sensorAlumbrado.getTiempoApagadoEncendido() == 11
+					|| sensorAlumbrado.getTiempoApagadoEncendido() == 13) {
+				sensorAlumbrado.getAlumbrado().setControlDeAlumbrado("Funcionando Correctamente");
+			} else {
+				sensorAlumbrado.getAlumbrado().setControlDeAlumbrado("Mantenimiento Requerido");
+			}
+
 			evento.setFechaHoraRegistro(LocalDateTime.now());
-			evento.setDescripcion(sensorAlumbrado.toString());
+			evento.setDescripcion("Estado de las luces: " + sensorAlumbrado.getAlumbrado().isEstadoLuces()
+					+ " Control de Alumbrado: " + sensorAlumbrado.getAlumbrado().getControlDeAlumbrado());
 			evento.setDispositivo(sensorAlumbrado.getAlumbrado());
 			eventoService.insertOrUpdate(evento);
 		}
