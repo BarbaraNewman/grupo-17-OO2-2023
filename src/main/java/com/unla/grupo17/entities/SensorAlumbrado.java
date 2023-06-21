fbae750629ae5015f5ac9573cef922db1a43e2b8
naylamarie <116470251+naylamarie@users.noreply.github.com>
2023-06-21 15:29:35 -0300

    Update SensorAlumbrado.java

--- src/main/java/com/unla/grupo17/entities/SensorAlumbrado.java
+++ src/main/java/com/unla/grupo17/entities/SensorAlumbrado.java
@@ -1,7 +1,5 @@
 package com.unla.grupo17.entities;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -23,7 +23,12 @@ public class SensorAlumbrado {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idSensor;
-	private LocalDateTime fechaHora;
+	
+	@Min(value = 0, message = "El nivel de luminosidad debe ser igual o mayor a 0.")
+	@Max(value = 100, message = "El nivel de luminosidad debe ser igual o menor a 100.")
+	private int umbralLuminosidad;
+	
+	private long tiempoApagadoEncendido;
 	
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "idAlumbrado", nullable = true)
