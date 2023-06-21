df2eeb4fc5f93f7fe126faf1349703b34eeccb7b
Barbara Newman <120697795+BarbaraNewman@users.noreply.github.com>
2023-06-20 23:44:09 -0300

    Create insert_metricas_parking.sql

---
+++ insert_metricas_parking.sql
@@ -0,0 +1,8 @@
+use `grupo-17-oo2-2023`;
+
+insert into `grupo-17-oo2-2023`.medicion_parking(id_medicion_parking, created_at, hora, ocupado,
+id_dispositivo)
+values
+(2, '2023-06-20 09:00:00', '2023-06-20 09:00:00', false, 2),
+(3, '2023-06-20 12:00:00', '2023-06-20 12:00:00', true, 2),
+(4, '2023-06-20 22:58:00', '2023-06-20 22:58:00', false, 2);
