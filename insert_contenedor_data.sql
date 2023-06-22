846842cbfd104546c3119f40904ff7553b469b94
Sergio Robles <mpthrash@gmail.com>
2023-06-21 23:24:15 -0300

    Cambio de datos en scripts.sql
    Update:
    insert_contenedor_data.sql
    insert_dispositivo_data.sql
    insert_sensor_contenedor_data.sql

--- insert_contenedor_data.sql
+++ insert_contenedor_data.sql
@@ -3,8 +3,8 @@ USE `grupo-17-oo2-2023`;
 INSERT INTO contenedor
 (capacidad, reciclable, id_dispositivo)
 VALUES
-(100, true, 1),
-(200, false, 2),
-(150, true, 3),
-(250, false, 4),
-(180, true, 5);
+(2400, true, 1),
+(1200, false, 2),
+(600, true, 3),
+(1200, false, 4),
+(2400, true, 5);
