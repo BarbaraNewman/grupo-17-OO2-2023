176d2e5c5fa38bbdd9dc98fe6b4c0a43496d62d4
Sergio Robles <mpthrash@gmail.com>
2023-06-22 19:00:53 -0300

    Update ContenedorModel.java

--- src/main/java/com/unla/grupo17/models/ContenedorModel.java
+++ src/main/java/com/unla/grupo17/models/ContenedorModel.java
@@ -8,8 +8,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ContenedorModel extends DispositivoModel {
+	private int idDispositivo;
 	private boolean reciclable;
 	private int capacidad;
 	private boolean lleno;
+
+	public ContenedorModel(int idDispositivo, boolean reciclable, int capacidad, boolean lleno) {
+		this.setIdDispositivo(idDispositivo);
+		this.reciclable = reciclable;
+		this.capacidad = capacidad;
+		this.lleno = lleno;
+	}
 
 }
