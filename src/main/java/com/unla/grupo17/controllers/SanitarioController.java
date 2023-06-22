4ddff1adcf756c5d36f408d420966210f0378973
LucianoSpinazzola <35580274+LucianoSpinazzola@users.noreply.github.com>
2023-06-22 19:21:57 -0300

    Update SanitarioController.java

--- src/main/java/com/unla/grupo17/controllers/SanitarioController.java
+++ src/main/java/com/unla/grupo17/controllers/SanitarioController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.unla.grupo17.entities.Sanitario;
 import com.unla.grupo17.helpers.ViewRouteHelper;
+import com.unla.grupo17.services.ISanitarioService;
 
 @Controller
 @RequestMapping("/dispositivo/sanitario")
@@ -23,8 +26,12 @@ public class SanitarioController {
 	@GetMapping("")
 	public ModelAndView index() {
 		ModelAndView mAV = new ModelAndView(ViewRouteHelper.SANITARIO_INDEX); // Vista
+		// Obtencion del nombre de Usuario
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = auth.getName();
+		mAV.addObject("username", username);
 
-		List<Sanitario> sanitario = sanitarioService.getAll();
+		List<Sanitario> sanitarios = sanitarioService.getAll();
 
 		mAV.addObject("sanitarios", sanitarios);
 		mAV.addObject("sanitario", new Sanitario());
