e6bdf1bf84abcd909488d497d14ffd7909c9df42
LucianoSpinazzola <35580274+LucianoSpinazzola@users.noreply.github.com>
2023-06-26 11:01:12 -0300

    Update Contenedor.java

--- src/main/java/com/unla/grupo17/entities/Contenedor.java
+++ src/main/java/com/unla/grupo17/entities/Contenedor.java
@@ -1,6 +1,9 @@
 package com.unla.grupo17.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,8 +13,14 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Contenedor extends Dispositivo {
+	@Column(columnDefinition = "boolean default false")
 	private boolean reciclable;
+
+	@NotNull(message = "La capacidad no puede ser nula")
+	@Min(value = 1, message = "La capacidad debe ser mayor o igual a 1")
 	private int capacidad;
+
+	@Column(columnDefinition = "boolean default false")
 	private boolean lleno;
 
 }
