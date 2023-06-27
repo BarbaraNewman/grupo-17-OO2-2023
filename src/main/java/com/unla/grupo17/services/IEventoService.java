b11c6968e3c4ce24ed1c6aefe2382126a695ec33
LucianoSpinazzola <35580274+LucianoSpinazzola@users.noreply.github.com>
2023-06-27 10:49:58 -0300

    Update IEventoService.java

--- src/main/java/com/unla/grupo17/services/IEventoService.java
+++ src/main/java/com/unla/grupo17/services/IEventoService.java
@@ -2,14 +2,32 @@ package com.unla.grupo17.services;
 
 import java.util.List;
 
+import com.unla.grupo17.entities.Dispositivo;
 import com.unla.grupo17.entities.Evento;
+import com.unla.grupo17.entities.Ubicacion;
 
 public interface IEventoService {
 
-	List<Evento> getAll();
+	public abstract List<Evento> getAll();
+
+	public abstract void deleteAll();
+
+	public abstract List<Evento> getAllByOrderByFechaHoraRegistroDesc();
 
 	public abstract Evento findByIdEvento(int idEvento);
 
-	public Evento insertOrUpdate(Evento evento);
+	public abstract Evento insertOrUpdate(Evento evento);
+
+	public abstract List<Evento> findByUltimoIdDispositivoEvento(int idDispositivo);
+
+	public abstract List<Evento> getEventosByUbicacion(Ubicacion ubicacion);
+
+	public abstract List<Evento> getEventosByUbicacion(int idUbicacion);
+
+	public abstract List<Evento> getEventosByDispositivo(Dispositivo dispositivo);
+
+	public abstract List<Evento> getEventosByDispositivo(int idDispositivo);
+
+	public abstract List<Evento> getEventosByDispositivoActivo(boolean activo);
 
 }
