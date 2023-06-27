eb124c9e861a3fc912cf206e8d988a4b5598766f
LucianoSpinazzola <35580274+LucianoSpinazzola@users.noreply.github.com>
2023-06-27 11:35:38 -0300

    Add files via upload

---
+++ insert_sensor_alumbrado_data.sql
@@ -0,0 +1,10 @@
+USE `grupo-17-oo2-2023`;
+
+INSERT INTO sensor_alumbrado
+(id_sensor,tiempo_apagado_encendido, umbral_luminosidad, id_alumbrado)
+VALUES
+(1, 11, 20, 1),
+(2, 13, 80, 2),
+(3, 40, 18, 3),
+(4, 0, 85, 4),
+(5, 11, 16, 5);
