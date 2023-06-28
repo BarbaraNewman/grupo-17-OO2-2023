03daa33c63e52d620206dae5da4d8412cdb34163
LucianoSpinazzola <35580274+LucianoSpinazzola@users.noreply.github.com>
2023-06-27 21:07:55 -0300

    Update Sanitario.java

--- src/main/java/com/unla/grupo17/entities/Sanitario.java
+++ src/main/java/com/unla/grupo17/entities/Sanitario.java
@@ -1,16 +1,26 @@
 package com.unla.grupo17.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter @Setter @NoArgsConstructor
-public class Sanitario extends Dispositivo{
-private String nroDeLugar;
+@Getter
+@Setter
+@NoArgsConstructor
+public class Sanitario extends Dispositivo {
+	@NotNull(message = "este campo es requerido.")
+	private String nroDeLugar;
+	@NotNull(message = "Este campo es requerido.")
 private int cantPersonas;
-private int cantLabatorios;
-private int nroPuertas;
+	@Min(value = 0, message = "Numero de labatorios debe ser mayor a 0.")
+
+	private int cantLabatorios;
+	@Min(value = 0, message = "Numero de puertas debe ser mayor a 0.")
+
+	private int nroPuertas;
 
 }
